677186bddef9e4f607ae2e4cb1977cb9af5f5d50
Fenzslerx <Fieldjullajak@gmail.com>
2026-01-30 04:53:57 +0700

    sdfddd

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useRef, useEffect } from "react"
 import { useRouter } from "next/navigation"
-import MultiImageUpload from '@/components/MultiImageUpload'
+
 
 export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
